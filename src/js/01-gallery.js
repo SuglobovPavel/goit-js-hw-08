6b12baf9be76885c769e72a3bf6352fb3729ac45
Pavel <parbeck17@gmail.com>
2022-05-07 18:06:42 +0300

    finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+
+const boxForGallery = document.querySelector(".gallery");
+function renderGallery(images){
+   
+   let galleryContent = ``;
+
+   images.map( ({preview, description, original}) => {
+      galleryContent += `
+      <a class="gallery__item" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+   })
+
+   boxForGallery.innerHTML = galleryContent;
+}
+renderGallery(galleryItems);
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+   captionsData: 'alt',
+   captionDelay: '250'
+});
